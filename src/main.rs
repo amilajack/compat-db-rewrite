9980b4bb78c3f208762274dd303d58fa2a21bd9f
Amila Welihinda <amilajack@gmail.com>
2017-11-24 21:24:39 -0800

    Added initial type information

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 extern crate rusqlite;
 extern crate time;
 
-use time::Timespec;
 use rusqlite::Connection;
 
-#[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-    time_created: Timespec,
-    data: Option<Vec<u8>>
+enum ApiType {
+    value,
+    attr,
+    tag
+}
+
+enum AstNodeTypes {
+    MemberExpression,
+    NewExpression,
+    CallExpression
+}
+
+struct Record {
+    api_type: ApiType,
+    ast_node_types: AstNodeTypes
+}
+
+struct DatabaseRecord {
+    agents: String,
+    records: Vec<Record>
 }
 
 fn main() {
@@ -27,28 +40,4 @@ fn main() {
         )",
         &[]
     ).unwrap();
-
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        time_created: time::get_time(),
-        data: None
-    };
-    conn.execute("INSERT INTO person (name, time_created, data)
-                  VALUES (?1, ?2, ?3)",
-                 &[&me.name, &me.time_created, &me.data]).unwrap();
-
-    let mut stmt = conn.prepare("SELECT id, name, time_created, data FROM person").unwrap();
-    let person_iter = stmt.query_map(&[], |row| {
-        Person {
-            id: row.get(0),
-            name: row.get(1),
-            time_created: row.get(2),
-            data: row.get(3)
-        }
-    }).unwrap();
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
 }
