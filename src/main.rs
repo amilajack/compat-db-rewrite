52ebf2678ab6342916a8a4a9f7dda157dbd195c3
Amila Welihinda <amilajack@gmail.com>
2017-11-25 10:03:37 -0800

    Added missing deps

--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,44 @@ extern crate serde_json;
 extern crate serde_derive;
 
 use serde_json::Error;
-
+use std::fs::File;
+use std::io::prelude::*;
 use rusqlite::Connection;
 
-enum ApiType {
-    value,
-    attr,
-    tag
-}
-
+#[derive(Serialize, Deserialize)]
 enum AstNodeTypes {
     MemberExpression,
     NewExpression,
     CallExpression
 }
 
+#[derive(Serialize, Deserialize)]
 struct Record {
-    astNodeTypes: AstNodeTypes,
+    // astNodeTypes: AstNodeTypes,
     id: String,
     name: String,
     protoChain: Vec<String>,
     protoChainId: String,
     specIsFinished: bool,
-    apiType: ApiType
+    apiType: String
 }
 
 struct DatabaseRecord {
     agents: String,
     records: Vec<Record>
 }
-
-fn parseRecords() {
-    #[derive(Serialize, Deserialize)]
-
-}
-
+// #[derive(Debug)]
 fn main() {
+    let filename = "records.json";
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    let p: Vec<Record> = serde_json::from_str(&contents).expect("adfs");
+    println!("{}", p.len());
+
     // Temporarily using rustqlite. Ideally, we could  migrate to desil so
     // we have a higher level API and we're not tied to one database.
     // Migration to MYSQL is ideal since we can create a pool of connections.
